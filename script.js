870ecde90bd04acdc4e52530f6a6c32eaea6be06
Nate <daviesnathanael45@gmail.com>
2025-11-21 14:44:52 +0000

    added contact page, there is also now a section that is my current project im wokring on by calling the latest repo from github.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+fetch("https://api.github.com/users/ex0Rift/repos?sort=updated&direction=desc")
+    .then(r => r.json())
+    .then(repos => {
+        const latest = repos[0];
+        console.log(latest);
+        document.getElementById("current-project-title").textContent = latest.name;
 
+        var language = document.getElementById("current-project-language");
+        language.textContent = latest.language;
+        if (language.textContent === "HTML"){
+            language.className = "cyan";
+        }else if (language.textContent === "Python"){
+            language.className = "blue";
+        }else if (language.textContent === "C"){
+            language.className = "yellow";
+        }
 
+        document.getElementById("current-project-description").textContent = latest.description;
 
+        document.getElementById("current-project-link").href = latest.html_url;
+
+        //for push date
+        const pushed = new Date(latest.pushed_at);
+        const now = new Date();
+        const difftmp = now - pushed;
+        const diffDays = Math.floor(difftmp / (1000 * 60 * 60));
+
+        document.getElementById("current-project-time-since-text").textContent = diffDays + " hours ago";
+    });
